fad946ca7fa838104dca09acd2eee50f372a9a81
miamar <mia.markanovic16@gmail.com>
2023-06-30 17:32:19 +0200

    header

--- static/js/header.js
+++ static/js/header.js
@@ -1,70 +1,69 @@
-var app = new Vue({
-    el: 'header',
-    data: {
-      openDropdown: ''
+
+
+var headerApp = new Vue({
+  el: 'header',
+  data: {
+    openDropdown: ''
+  },
+  methods: {
+    toggleDropdown(dropdown) {
+      this.openDropdown = this.openDropdown === dropdown ? '' : dropdown;
+      if (this.openDropdown) {
+        document.addEventListener('click', this.closeDropdown);
+      } else {
+        document.removeEventListener('click', this.closeDropdown);
+      }
     },
-    methods: {
-      toggleDropdown(dropdown) {
-        this.openDropdown = this.openDropdown === dropdown ? '' : dropdown;
-        if (this.openDropdown) {
-          // Attach a click event listener to the document
-          document.addEventListener('click', this.closeDropdown);
-        } else {
-          // Remove the click event listener from the document
-          document.removeEventListener('click', this.closeDropdown);
-        }
-      },
-      isDropdownOpen(dropdown) {
-        return this.openDropdown === dropdown;
-      },
-      closeDropdown(event) {
-        // Check if the click event target is within the header element
-        if (!event.target.closest('header')) {
-          this.openDropdown = '';
-          // Remove the click event listener from the document
-          document.removeEventListener('click', this.closeDropdown);
-        }
+    isDropdownOpen(dropdown) {
+      return this.openDropdown === dropdown;
+    },
+    closeDropdown(event) {
+      if (!event.target.closest('header')) {
+        this.openDropdown = '';
+        document.removeEventListener('click', this.closeDropdown);
       }
     }
-  });
+  }
+});
 
-  jQuery('img.svg').each(function(){
-    var $img = jQuery(this);
-    var imgID = $img.attr('id');
-    var imgClass = $img.attr('class');
-    var imgURL = $img.attr('src');
-  
-    jQuery.get(imgURL, function(data) {
-        // Get the SVG tag, ignore the rest
-        var $svg = jQuery(data).find('svg');
-  
-        // Add replaced image's ID to the new SVG
-        if(typeof imgID !== 'undefined') {
-            $svg = $svg.attr('id', imgID);
-        }
-        // Add replaced image's classes to the new SVG
-        if(typeof imgClass !== 'undefined') {
-            $svg = $svg.attr('class', imgClass+' replaced-svg');
-        }
-  
-        // Remove any invalid XML tags as per http://validator.w3.org
-        $svg = $svg.removeAttr('xmlns:a');
-  
-        // Replace image with new SVG
-        $img.replaceWith($svg);
-  
-    }, 'xml');
-  
-  });
+
+jQuery('img.svg').each(function(){
+var $img = jQuery(this);
+var imgID = $img.attr('id');
+var imgClass = $img.attr('class');
+var imgURL = $img.attr('src');
+
+jQuery.get(imgURL, function(data) {
+    // Get the SVG tag, ignore the rest
+    var $svg = jQuery(data).find('svg');
+
+    // Add replaced image's ID to the new SVG
+    if(typeof imgID !== 'undefined') {
+        $svg = $svg.attr('id', imgID);
+    }
+    // Add replaced image's classes to the new SVG
+    if(typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass+' replaced-svg');
+    }
+
+    // Remove any invalid XML tags as per http://validator.w3.org
+    $svg = $svg.removeAttr('xmlns:a');
+
+    // Replace image with new SVG
+    $img.replaceWith($svg);
+
+}, 'xml');
+
+});
 
 $(document).ready(function() {
-  $(".navbar .nav-link").focus(function() {
-    $(".navbar").addClass("navbar-focused");
-  });
+$(".navbar .nav-link").focus(function() {
+  $(".navbar").addClass("navbar-focused");
+});
 
-  $(".navbar .nav-link").blur(function() {
-    $(".navbar").removeClass("navbar-focused");
-  });
+$(".navbar .nav-link").blur(function() {
+  $(".navbar").removeClass("navbar-focused");
+});
 });
 
 // Add scroll event handler
