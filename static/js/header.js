6f39e557f4f1655a3d57674cf9e1469a40c942f5
miamar <mia.markanovic16@gmail.com>
2023-06-30 16:56:40 +0200

    header

--- static/js/header.js
+++ static/js/header.js
@@ -1,6 +1,4 @@
-
-
-  var app = new Vue({
+var app = new Vue({
     el: 'header',
     data: {
       openDropdown: ''
@@ -30,34 +28,34 @@
     }
   });
 
-jQuery('img.svg').each(function(){
-  var $img = jQuery(this);
-  var imgID = $img.attr('id');
-  var imgClass = $img.attr('class');
-  var imgURL = $img.attr('src');
-
-  jQuery.get(imgURL, function(data) {
-      // Get the SVG tag, ignore the rest
-      var $svg = jQuery(data).find('svg');
-
-      // Add replaced image's ID to the new SVG
-      if(typeof imgID !== 'undefined') {
-          $svg = $svg.attr('id', imgID);
-      }
-      // Add replaced image's classes to the new SVG
-      if(typeof imgClass !== 'undefined') {
-          $svg = $svg.attr('class', imgClass+' replaced-svg');
-      }
-
-      // Remove any invalid XML tags as per http://validator.w3.org
-      $svg = $svg.removeAttr('xmlns:a');
-
-      // Replace image with new SVG
-      $img.replaceWith($svg);
-
-  }, 'xml');
-
-});
+  jQuery('img.svg').each(function(){
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+  
+    jQuery.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find('svg');
+  
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+  
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+  
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+  
+    }, 'xml');
+  
+  });
 
 $(document).ready(function() {
   $(".navbar .nav-link").focus(function() {
@@ -67,5 +65,14 @@ $(document).ready(function() {
   $(".navbar .nav-link").blur(function() {
     $(".navbar").removeClass("navbar-focused");
   });
+});
+
+// Add scroll event handler
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 0) {
+    $(".navbar").addClass("navbar-scrolled");
+  } else {
+    $(".navbar").removeClass("navbar-scrolled");
+  }
 });
 
